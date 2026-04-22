a5432ea7d8f02a10bcdc0e07a10c044932d54151
Guilherme Almeida <gui.starkteam@gmail.com>
2026-04-22 20:38:03 -0300

    feat: crud completo com responseentity e busca por id

--- src/main/java/com/guiverme/meu_backlog_games/GameController.java
+++ src/main/java/com/guiverme/meu_backlog_games/GameController.java
@@ -1,8 +1,14 @@
 package com.guiverme.meu_backlog_games;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Path;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/jogos")
@@ -17,7 +23,32 @@ public class GameController {
     }
 
     @PostMapping
-    public Game cadastrar(@RequestBody Game novoJogo) {
-        return repository.save(novoJogo);
+    public ResponseEntity<Game> cadastrar(@RequestBody Game novoJogo) {
+        Game jogoSalvo = repository.save(novoJogo);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(jogoSalvo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Game> atualizar(@PathVariable Long id, @RequestBody Game jogoAtualizado) {
+        return repository.findById(id).map(game -> {
+            game.setTitulo(jogoAtualizado.getTitulo());
+            game.setPlataforma(jogoAtualizado.getPlataforma());
+            game.setNota(jogoAtualizado.getNota());
+            game.setStatus(jogoAtualizado.getStatus());
+            return ResponseEntity.ok(repository.save(game));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> buscarPorId(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(game -> ResponseEntity.ok(game))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
